4826ea24d4ab632c1a030f3399f425ca58f1c035
mvignere <mvignere@JFC-221819L>
2024-03-07 09:34:44 +0100

    rangee(Color c) : on peut ajouter couleurs par couleurs

--- src/Rangee.java
+++ src/Rangee.java
@@ -1,4 +1,5 @@
 import java.awt.Color;
+import java.util.Iterator;
 import java.util.Random;
 
 public class Rangee {
@@ -17,6 +18,19 @@ public class Rangee {
 	
 	public Rangee(Color c1, Color c2, Color c3, Color c4) {
 		this.jetons[0] = c1;
+		this.jetons[1] = c2;
+		this.jetons[2] = c3;
+		this.jetons[3] = c4;
+	}
+	
+	public boolean Rangee(Color c) {
+		if (this.indiceJeton == 4) {
+			return false; //Signifie qu'il faut créer une nouvelle rangée
+		} else {
+			this.jetons[this.indiceJeton] = c;
+			this.indiceJeton++;
+			return true;
+		}
 	}
 	
 	public String toString() {
@@ -27,7 +41,7 @@ public class Rangee {
 		phrase.append(" | ");
 		return phrase.toString();
 	}
-	
+
 	
 
 }
