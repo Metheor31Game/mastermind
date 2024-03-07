4fc3efb4220f2de77e46f843a9743ea9386d137b
mvignere <mvignere@JFC-221819L>
2024-03-07 09:00:42 +0100

    fonction ajouterProposition(Rangee r)

--- src/Modele.java
+++ src/Modele.java
@@ -1,5 +1,6 @@
 import java.awt.Color;
 import java.util.ArrayList;
+import java.util.Random;
 
 public class Modele {
 	public static Color[] COULEURS = {Color.yellow,Color.green,Color.blue,Color.magenta,Color.red,Color.orange,Color.white,Color.black};
@@ -19,15 +20,31 @@ public class Modele {
 		this.propositions = new ArrayList<>();
 	}
 	
+	public void ajouterProposition(Rangee r) {
+		/*Ne pas oublier de vérifier si il reste des tentatives, mais je sais pas si je l'ajoute maintenant ou non */
+		if (this.tentative < Modele.N_TENTATIVES) {
+			this.propositions.add(r);
+			this.tentative-=1;
+			// TODO Appel fonction de vérification
+		} else {
+			// TODO Appeler fonction de defaite
+		}
+	}
+	
 	public String toString() {
 		StringBuilder phrase = new StringBuilder();
 		/*phrase.append("Derniere proposition : " + this.propositions.get(this.propositions.size()-1));*/
 		phrase.append("\nRésultat : " + this.combinaisonInitiale);
+		phrase.append("\nPropositions : \n" + this.propositions.toString());
 		return phrase.toString();
 	}
 	
 	public static void main(String[] args) {
 		Modele m = new Modele();
+		
+		System.out.println(m);
+		
+		m.ajouterProposition(new Rangee(Color.yellow,Color.green,Color.blue,Color.red));
 		
 		System.out.println(m);
 		
